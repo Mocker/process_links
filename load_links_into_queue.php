a1092091da2e280b7d9fdc000ed741c7117dd014
rsx491 <rsx491@rsx491s-MBP.attlocal.net>
2020-03-11 13:38:18 -0400

    rabbitmq prod/con updates for home details

--- load_links_into_queue.php
+++ load_links_into_queue.php
@@ -21,6 +21,7 @@ $channel = $connection->channel();
 $channel->queue_declare($queue, false, false, false, false);
 
 $links = (new MongoDB\Client("mongodb://localhost:27017"))->zillow->listings;
+$home_details = (new MongoDB\Client("mongodb://localhost:27017"))->zillow->homes;
 
 $json = file_get_contents('./zip_codes_long.json');
 $zip_codes = json_decode($json, true);
@@ -31,8 +32,18 @@ foreach($florida as $row){
 	$record = $links->findOne(array('_id' => $zip), ['projection' => ['Homes' => 1]]);
 	
 	foreach($record->Homes as $url){
-		$msg = new AMQPMessage("$zip");
-		$channel->basic_publish($msg, '', $queue);
+		$a = explode("/", $url);
+		$fname = "{$a[4]}";
+		if(strpos($url, "community")){
+			$fname .= "-{$a[5]}";
+		}
+		$count = $home_details->count(array('_id' => $index ), []);
+		
+		// filter worker input accurately 
+		if($count == 0){
+			$msg = new AMQPMessage("$url@$zip");
+			$channel->basic_publish($msg, '', $queue);
+		}
 	}
 }
 
