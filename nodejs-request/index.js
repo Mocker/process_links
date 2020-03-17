d36bd48e651ec6f67d4a53b5e273ed989be3ca55
Ryan Guthrie <ryan@ryanguthrie.com>
2020-03-16 20:22:16 -0600

    Handle multiple proxies better and logging. Tested with 1000 urls so far

--- nodejs-request/index.js
+++ nodejs-request/index.js
@@ -1,5 +1,5 @@
 const util = require('util');
-const spawn = require('child_process').spawn;
+const { exec, spawn } = require('child_process');
 const request = require('superagent');
 const lineByLine = require('n-readlines');
 const fs = require('fs');
@@ -10,7 +10,10 @@ const config = {
     // URL of the site
     //https://www.zillow.com/homedetails/577-Mulberry-Dr-Fleming-Island-FL-32003/43701102_zpid/
     uri: 'https://ifconfig.me/ip',
-    listings_file: '../mongo/zillow/listings.json',
+    url_dir: '../urls', //scan all files in the directory
+    url_order: 'desc',
+    url_random: 'true',
+    //url_file: '../mongo/zillow/listings.json',
     listings_dir: '../listings/',
     // URL of Scrapoxy
     proxy: 'http://localhost:8888',
@@ -18,36 +21,55 @@ const config = {
     proxyAuth: 'Y3Jhd2xfbGlua3M=',
     // HTTPS over HTTP
     tunnel: false,
-    
+    log_dir: '../logs/',
 };
 
-const MIN_PROXIES_REQUIRED=1;
+const MIN_PROXIES_REQUIRED=3;
 let proxies = [];
-const MAX_ACTIVE_REQUESTS=1;
+const MAX_ACTIVE_REQUESTS=4;
 let activeRequests = 0;
-const MAX_LISTINGS_READ=1;
+const MAX_LISTINGS_READ=1000;
 let listingsRead = 0;
-const MAX_ZIPS_READ=4;
+const MAX_ZIPS_READ=1000;
 let zipsRead = 0;
 const USER_AGENT_IOS = '-A "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5"';
-const MAX_FAILED_IPS=1;
+const MAX_FAILED_IPS=15;
 let failed_ips=0;
+let existing_listings=0;
 
 const start = Date.now();
+const log_file = config.log_dir+(new Date()).toLocaleString().replace(/[^\d\:\w]/g,'_')+'.log';
+let liner = null;
 
+let url_files = [];
+let file_i = -1;
+let curl_logs = [];
 
+fs.appendFileSync(log_file, `Beginning run ${MIN_PROXIES_REQUIRED} min proxies, ${MAX_LISTINGS_READ} max_listings, ${MAX_ZIPS_READ} max zips, ${MAX_ACTIVE_REQUESTS} active requests. ${url_files.length} total json files to parse\n`,'utf8');
 
 console.log("Checking there are available proxies..");
 waitForProxies( async (res) => {
+    url_files = await new Promise( (resolve, reject) => {
+        fs.readdir(config.url_dir, (err, items) => {
+            if(err) throw(err);
+            resolve(items);
+        });
+    });
+    if(config.url_random){
+        url_files = shuffle(url_files);
+    }
+    file_i = (config.url_order=='desc') ? url_files.length  : -1;
+    fs.appendFileSync(log_file, ` ${url_files.length} total json files to parse\n`,'utf8');
     proxies = res;
     let curProxy = 0;
-    const liner = new lineByLine(config.listings_file);
-    let line= liner.next();
+    fs.appendFileSync(log_file, `Proxies ready: ${JSON.stringify(proxies)}\n`,'utf8');
+    line = getNextLine();
     while( zipsRead++ < MAX_ZIPS_READ && listingsRead < MAX_LISTINGS_READ && failed_ips < MAX_FAILED_IPS && line  ) {
         zip = JSON.parse(line);
         dir = config.listings_dir+zip.Zip;
         if(!fs.existsSync(dir) ) fs.mkdirSync(dir);
         console.log("Beginning zip",zip.Zip,zip.Homes.length);
+        fs.appendFileSync(log_file, `Beginning zip ${zip.Zip} with ${zip.Homes.length} listings\n`, 'utf8');
         while(zip.Homes.length > 0 && listingsRead < MAX_LISTINGS_READ && failed_ips < MAX_FAILED_IPS) {
             const url = zip.Homes.pop();
             const a = url.split('/');
@@ -58,9 +80,11 @@ waitForProxies( async (res) => {
             fname = dir + '/'+fname;
             if(fs.existsSync(fname)){
                 console.log("Exists "+fname);
+                existing_listings++;
                 continue;
             }
-            console.log("Next url", url, '#'+listingsRead, proxies.length+' > '+MIN_PROXIES_REQUIRED+' proxies. ', activeRequests+' / '+MAX_ACTIVE_REQUESTS+' requests. ');
+            console.log(`Next url ${url} # ${listingsRead, proxies.length} > ${MIN_PROXIES_REQUIRED} proxies. ${activeRequests} / ${MAX_ACTIVE_REQUESTS} requests. ` );
+            curl_logs.push( `Next url ${url} # ${listingsRead, proxies.length} > ${MIN_PROXIES_REQUIRED} proxies. ${activeRequests} / ${MAX_ACTIVE_REQUESTS} requests. `);
             while( proxies.length < MIN_PROXIES_REQUIRED ) {
                 const res = await getProxyStatus();
                 proxies = res.body;
@@ -73,6 +97,24 @@ waitForProxies( async (res) => {
             listingsRead++;
             if(curProxy >= proxies.length) curProxy = 0;
             console.log(proxies[curProxy].address);
+            let waitForProxyCounter = 0;
+            if(!proxies[curProxy].address || !proxies[curProxy].address.hostname){
+                while( !proxies[curProxy].address || !proxies[curProxy].address.hostname ) {
+                    waitForProxyCounter++;
+                    if(waitForProxyCounter > proxies.length) {
+                        break;
+                    }
+                    curProxy++;
+                    if(curProxy >= proxies.length) curProxy = 0;
+                    sleep(5000);
+                }
+            }
+            if(waitForProxyCounter > proxies.length) {
+                console.log("Could not find any proxies with an active IP\n");
+                curl_logs.push(`Could not find any proxies with an active IP. Stopping the job\n`);
+                failed_ips = MAX_FAILED_IPS;
+                break;
+            }
             let curl = spawn('curl', 
                 ['-L','-s','-x','http://'+proxies[curProxy].address.hostname+':'+proxies[curProxy].address.port,'-A',USER_AGENT_IOS,'-D',fname+'.headers','-o',fname+'.html',url],
                 { });
@@ -89,6 +131,7 @@ waitForProxies( async (res) => {
                         lineCounter++;
                         if(lineCounter==3){
                             console.log("status "+usedProxy.name+": ",line);
+                            curl_logs.push( `[${line}] : ${zip.Zip} - ${fname} - ${usedProxy.name} - ${usedProxy.address.hostname}` );
                             fs.writeFile(fname+'.proxy', JSON.stringify(usedProxy), (err) => {
                                 if(err) console.log("Could not write proxy info to "+fname+'.proxy - ',err);
                             })
@@ -108,8 +151,19 @@ waitForProxies( async (res) => {
             });
             curProxy++;
         }
-        line = liner.next();
+        line = getNextLine();
     }
+    for( line in curl_logs) {
+        fs.appendFileSync(log_file, curl_logs[line]+"\n", 'utf8');
+    }
+    let folderSize = await new Promise( (resolve, reject) => {
+        exec(' du -mh '+config.listings_dir, (err, stdout, stderr) => {
+            if(err) reject(err);
+            resolve(stdout);
+        });
+    });
+    fs.appendFileSync(log_file, `Finished read ${listingsRead} urls in ${Date.now()-start} ms and size ${folderSize} - ${(Date.now()-start)/listingsRead} ms/listings - ${parseInt(folderSize.replace(/[^\d\.]/,''))/(listingsRead+existing_listings)} size/listing\n`, 'utf8');
+    if(liner) liner.close();
     console.log("Waiting for any active requests to finish up");
     while(activeRequests > 0 ){
         await sleep(1000);
@@ -118,6 +172,38 @@ waitForProxies( async (res) => {
     
 });
 
+function getNextLine(){
+    let line = null;
+    if( !liner || !(line = liner.next() ) ){
+        if(config.url_order=='desc'){
+            file_i--;
+            if(file_i >= 0) {
+                liner = new lineByLine(config.url_dir+'/'+url_files[file_i]);
+            } else {
+                return null;
+            }
+        } else if(config.url_order=='asc'){
+            file_i++;
+            if(file_i < url_files.length) {
+                liner = new lineByLine(config.url_dir+'/'+url_files[file_i]);
+            } else {
+                return null;
+            }
+        }
+        fs.appendFile(log_file, `Began parsing new json file # ${file_i} - ${url_files[file_i]}`,'utf8',(err)=>{});
+        
+    }
+    return liner.next();
+    
+}
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
 
 function waitForProxies(cb){
     getProxyStatus().then( (res) => {
